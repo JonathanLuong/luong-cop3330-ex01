20d54947647c179ab788dffbec7dc7958479ae12
Jonathan Luong <jonathanluong1023@gmail.com>
2021-09-13 16:24:54 -0700

    Exercise 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,20 @@
+package org.example;
 /*
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
  *  Copyright 2021 Jonathan Luong
  */
 
-package org.example;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner reader = new Scanner(System.in); // Creates reader
+
+        System.out.println( "What is your name? " );
+        String name = reader.nextLine(); // Takes in the name
+
+        System.out.println( "Hello, " + name + " nice to meet you!");
     }
 }
